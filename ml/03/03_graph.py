a94a11c1e5823901da22c7c6809c0ee411ebd687
Kostenko Volodymyr <volodymyr.kostenko@easternpeak.com>
2024-10-21 09:10:15 +0300

    ml

--- ml/03/03_graph.py
+++ ml/03/03_graph.py
@@ -1,6 +1,7 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
 
-plt.plot([1, 2, 3, 4])
-plt.ylabel('some numbers')
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro')
+plt.axis([0, 6, 0, 20])
 plt.show()
-
