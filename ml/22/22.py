264402084208bb6e7562782e00fe37d1c29f1cb8
Volodymyr <volodymyr.kostenko@easternpeak.com>
2024-11-19 14:30:39 +0200

    22

--- ml/22/22.py
+++ ml/22/22.py
@@ -31,8 +31,6 @@ kmeans.fit(X)
 step_size = 0.01
 
 # Grid
-# x_min, x_max = X[:,0].min()-1, X[:,0].max()+1
-# y_min, y_max = X[:,1].min()-1, X[:,1].max()+1
 
 x_vals, y_vals = np.meshgrid(np.arange(x_min, x_max, step_size), np.arange(y_min, y_max, step_size))
 output = kmeans.predict(np.c_[x_vals.ravel(), y_vals.ravel()])
@@ -44,7 +42,7 @@ plt.clf()
 plt.imshow(output, interpolation='nearest', extent=( x_vals.min(), x_vals.max(), y_vals.min(), y_vals.max()), cmap=plt.cm.Paired, aspect='auto', origin='lower' )
 
 # 2
-plt.show()
+#plt.show()
 plt.scatter(X[:,0], X[:,1], marker='o', facecolor='none', edgecolor='black', s=80)
 
 # 3
@@ -52,7 +50,7 @@ plt.show()
 
 # 4 +Center of 
 cluster_centers = kmeans.cluster_centers_
-plt.scatter(cluster_centers[:,0], cluster_centers[:,1], marker='o', s=200, linewidths=4, color='black', zorder=12, facecolor='black')
+plt.scatter(cluster_centers[:,0], cluster_centers[:,1], marker='o', s=200, linewidths=4, color='gray', zorder=12, facecolor='black')
 plt.title('Borders of clusters')
 plt.xlim(x_min, x_max)
 plt.ylim(y_min, y_max)
